4c8c34ff812a3c027027782b0e4a7d2d067a4c45
s0st <roshantester1@gmail.com>
2025-03-31 14:32:07 +0530

    see

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_GEMINI_API_KEY: string;
+  readonly VITE_API_URL: string; // Add the API URL variable definition
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
-} 
+}
